f56838a4952231d828347adf1acce7fd21cbb17f
mechabubba <minecraftguy66@gmail.com>
2021-05-24 01:11:24 -0500

    massive update

--- ttt/userquotes.js
+++ ttt/userquotes.js
@@ -1,13 +1,15 @@
-/* the collection of submitted user quotes from the func.zone community */
-/* last updated: 09/20/20 */
-var userquotes = [
+/* A collection of submitted user quotes from the func.zone community. */
+let userquotes = [
     "\"the rare blue traitor\"",
-    "\"<a href='https://fuck.zone'>fuck.zone</a>\"", // credit: squall
-    "\"server is dying cause steven sucked his own dick too hard\"", // credit: bunnies
-    "\"detective car\"", // credit: chevin + cobb
+    "\"server is dying cause steven sucked his own dick too hard\"",
+    "\"detective car\"",
+    "\"fart gun\"",
     "\"dEaD sErVeR\"",
-    "\"Want to feature a quote or some other message here? Send it to Steven, and he'll add it! :D\"",
+    "\"Want to feature a quote or some other message here? Send it to Steven, and he'll (maybe) add it! :D\"",
     "<img src='../assets/ttt/man.png' style='height:16px;'>",
-    "\"@FuncZ on twitter!\""
+    "\"Not @FuncZ on twitter!\"",
+    "\"Does anyone actually read these?\"",
+    "\"Populate!\"",
+    "\"only func inside anime girls\""
 ];
 document.getElementById("quote").innerHTML = userquotes[Math.floor(Math.random() * userquotes.length)];
